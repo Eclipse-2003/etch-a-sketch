ed38f8afa8e726bba09b8a7568eafe9496b43c6e
Mason McCoy <106836922+Eclipse-2003@users.noreply.github.com>
2025-07-13 18:08:27 -0500

    add logic for sqaure sizing and make them into a grid

---
+++ function.js
@@ -0,0 +1,12 @@
+console.log("hello world!");
+const containerSize = 800;
+const container = document.querySelector("#container");
+
+
+for (let i = 0; i < 256; i ++) {
+    const div = document.createElement("div");
+    div.classList.add("squares");
+    const squareSize = containerSize / 16;
+    div.style.cssText = "background: red; height: " + squareSize + "px; width: " + squareSize + "px;";
+    container.appendChild(div);
+}
