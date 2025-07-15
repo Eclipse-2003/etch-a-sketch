341fd127970f06ef399b957e9da2d0ce3626a52e
Mason McCoy <106836922+Eclipse-2003@users.noreply.github.com>
2025-07-14 21:10:13 -0500

    add button to change grid size, add removeGrid function

--- function.js
+++ function.js
@@ -2,14 +2,34 @@ console.log("hello world!");
 const containerSize = 800;
 const container = document.querySelector("#container");
 
+const resizeButton = document.querySelector("button")
 
-for (let i = 0; i < 256; i ++) {
-    const div = document.createElement("div");
-    div.classList.add("squares");
-    const squareSize = containerSize / 16;
-    div.style.cssText = "background: red; height: " + squareSize + "px; width: " + squareSize + "px;";
-    container.appendChild(div);
+resizeButton.addEventListener("click", () => {
+    let gridValue = prompt("Enter a number for the amount of squares for both sides. (Max 100)", "ex. 16")
+    if (gridValue) {
+        removeGrid();
+        createGrid(gridValue);
+    }
+})
+
+function createGrid (size) {
+    for (let i = 0; i < (size*size); i ++) {
+        const div = document.createElement("div");
+        div.classList.add("squares");
+        const squareSize = containerSize / size;
+        div.style.cssText = "height: " + squareSize + "px; width: " + squareSize + "px;";
+        container.appendChild(div);
+    }
 }
+
+function removeGrid () {
+    const sqaureRemoval = document.getElementById("container")
+    while (sqaureRemoval.firstChild) {
+        sqaureRemoval.removeChild(sqaureRemoval.firstChild);
+    }
+
+}
+
 const squares = document.getElementById("container");
 squares.addEventListener("mouseover", (event) => {
     event.target.style.backgroundColor = "blue";
@@ -18,3 +38,5 @@ squares.addEventListener("mouseover", (event) => {
     }, 500);
 }, false,
 );
+
+createGrid(16);
